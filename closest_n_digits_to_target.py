3b5d79c2d5b2fd4881e2bdd64fd8ac82f6f297f1
Séun Olarinre <44907572+obpsidian8@users.noreply.github.com>
2019-02-11 23:21:31 -0500

    Cleaned up the code

--- closest_n_digits_to_target.py
+++ closest_n_digits_to_target.py
@@ -11,52 +11,47 @@ def closest_n_digits_to_target(list_of_numbers, target, closest_n_numbers):
         print("Closest n digits must be less than list given")
         return
 
-    top_three_list = []
+    top_n_list = []
 
     print(f"\nLooking for closest {closest_n_numbers} numbers in the given list to {target}.\nProcessing list...")
 
     for number in list_of_numbers:
         input_number_distance_from_target = abs(number - target)
 
-        if not top_three_list:
+        if not top_n_list:
             # print("List is empty")
-            top_three_list.append(number)
+            top_n_list.append(number)
 
         else:
-            # print("Checking list contents")
-            for digit in top_three_list:
-                top_three_number_distance_to_target = abs(digit - target)
-                index_d = top_three_list.index(digit)
-                if input_number_distance_from_target <= top_three_number_distance_to_target:
-                    # print(f"Current number is closer to target then digit")
+            index_d = 0
+            while index_d <= len(top_n_list) - 1:
+                # While we are still within the bounds of the list
+                current_number_in_top_n = top_n_list[index_d]
+                current_number_in_top_n_distance_to_target = abs(current_number_in_top_n - target)
 
-                    top_three_list.insert(index_d, number)
-                    if len(top_three_list) > closest_n_numbers:
-                        top_three_list.pop()
+                if input_number_distance_from_target <= current_number_in_top_n_distance_to_target:
+                    # Current input number is closer to the target than the current number in the top n list.
+                    top_n_list.insert(index_d, number)
+                    break
+                elif index_d < len(top_n_list) - 1:
+                    # advance to next number in top n list to compare the distances
+                    index_d += 1
+                    continue
+                elif index_d == len(top_n_list)-1:
+                    # index has reached the end of the list. Append the current value being checked to the end of the list
+                    top_n_list.append(number)
                     break
 
-                else:
-                    if index_d < len(top_three_list)-1:
-                        continue
-
-                    elif index_d == len(top_three_list)-1 and len(top_three_list) < closest_n_numbers :
-                        top_three_list.append(number)
-                        break
-                    else:
-
-                        continue
-
-
-    while len(top_three_list)>closest_n_numbers:
-        top_three_list.pop()
+    while len(top_n_list) > closest_n_numbers:
+        top_n_list.pop()
 
     print("Processing done!")
-    return top_three_list
+    return top_n_list
 
 
 if __name__ == "__main__":
     list_of_numbers = [13, 2, 29, 6, 11, 17, 8, 9, 10, 16]
-    target = 10
+    target =11
     closest_n_numbers = 6
     result = closest_n_digits_to_target(list_of_numbers, target, closest_n_numbers)
     msg = f"\nThe closest {closest_n_numbers} numbers  to {target} are {result}"
